5ae9d8ff29a7e076b0321774ef5a48ccc1269404
yunl.zheng <yunl.zheng@wise2c.com>
2017-05-10 14:43:05 +0800

    add  profile control

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
+ACTIVE_PROFILE=${PROFILE:=default}
+echo 'ACTIVE PROFILE :'${ACTIVE_PROFILE}
+
+case ${ACTIVE_PROFILE} in
+'rancher')
+
+/bin/prometheus_pusher -config.customLabels=environmentUUID -config.customLabelValues=$(curl http://rancher-metadata/latest/self/host/environment_uuid) $@
+
+;;
+*)
+
 /bin/prometheus_pusher $@
+
+esac
+
